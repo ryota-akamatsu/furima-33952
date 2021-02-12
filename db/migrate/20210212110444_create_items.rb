71b6f54233ca4f77a6d149f09e852aa85d6575f6
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-02-12 20:21:54 +0900

    itemsテーブル作成

--- db/migrate/20210212110444_create_items.rb
+++ db/migrate/20210212110444_create_items.rb
@@ -1,6 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.text :description_item
+      t.string :title
+      t.integer :price
+      t.references :user,foreign_key: true
+      t.integer :category_id
+      t.integer :product_status_id
+      t.integer :shipping_id
+      t.integer :delivery_date_id
+      t.integer :area_id
+
 
       t.timestamps
     end
