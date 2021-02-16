e153c7adcb9f1534848c98c5f354fdc1b639a49f
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-02-16 14:57:35 +0900

    全角修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         with_options presence: true do
+         validates :birthday
+         validates :nickname
          validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-         validates :nickname, presence: true
          validates :last_name,presence: {message: 'last name cant be black'}
          validates :last_name_kana,presence: {message: 'last name kana cant be black'}
          validates :first_name,presence: {message: 'first name cant be black'}
          validates :first_name_kana,presence: {message: 'first name kana cant be black'}
-         validates :birthday,presence: true
+         
          validates :first_name,
+               format: {with:/\A[ぁ-んァ-ン一-龥]+\z/,message: 'first name cant be black'}
                :last_name,
+               format: {with:/\A[ぁ-んァ-ン一-龥]+\z/,message: 'first name cant be black'}
                :first_name_kana,
+               format: {with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/,message: 'Full-width characters'}
                :last_name_kana,
-               format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
+               format: {with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/,message: 'Full-width characters'}
         end
