5d1a7394d5c81e773ce8af2a712e37c9e6167f00
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-02-19 15:50:13 +0900

    編集画面実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except:[:index,:show]
+  before_action :edit_move_to_index,only:[:edit]
   def index
     @item = Item.all.order("created_at DESC")
   end
@@ -20,16 +21,27 @@ class ItemsController < ApplicationController
      @item = Item.find(params[:id])
   end
 
-  # def edit
-  #    @item = Item.find(params[:id])
-  # end
+  def edit
+     @item = Item.find(params[:id])
+   
+  end
 
-  # def update
-
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(items_params)
+    return redirect_to root_path 
+    else
+      render :edit
+    end
+  end
   private
   def items_params
     params.require(:item).permit(:image,:title,:delivery_date_id,:price,:category_id,:product_status_id,:shipping_id,:area_id,:description_item).merge(user_id: current_user.id)
   end
-
+  def edit_move_to_index
+    @item = Item.find(params[:id])
+      unless  current_user.id == @item.user_id
+        redirect_to action: :index
+    end
+  end
 end
