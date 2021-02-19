5cf0ce9a012745e4e637e8972f86fa5aa629e884
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-02-19 18:20:10 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except:[:index,:show]
   before_action :items_move,only:[:show,:edit,:update,:destroy]
-  before_action :edit_move_to_index,only:[:edit,:update]
+  before_action :edit_move_to_index,only:[:edit,:update,:destroy]
   def index
     @item = Item.all.order("created_at DESC")
   end
