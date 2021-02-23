71f98a13077d144a6c90c2559933013d6170b511
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-02-23 16:41:52 +0900

    購入機能再修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,7 +44,7 @@ class OrdersController < ApplicationController
   end
   def move_to_index
     @item = Item.find(params[:item_id])
-    if  @item.order != nil
+    if  @item.order != nil || current_user.id == @item.user_id
         redirect_to controller: :items, action: :index
     end
   end
